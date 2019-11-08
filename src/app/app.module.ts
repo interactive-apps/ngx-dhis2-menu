fb84b4da06470822fe6f5e6964a84e02793575c2
josephatJ <josephatjulius24@gmail.com>
2019-11-08 11:07:13 +0300

    hard-fix the missing messaging module url in the menu notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
-import { NgxDhis2HttpClientModule } from '@iapps/ngx-dhis2-http-client';
-import { NgxDhis2MenuModule } from '@iapps/ngx-dhis2-menu';
+import { AppComponent } from "./app.component";
+import { NgxDhis2HttpClientModule } from "@iapps/ngx-dhis2-http-client";
+import { NgxDhis2MenuModule } from "projects/ngx-dhis2-menu/src/public_api";
 
 @NgModule({
   declarations: [AppComponent],
